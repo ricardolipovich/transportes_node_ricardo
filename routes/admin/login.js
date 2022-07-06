b1bcfac0e28df7d3c05df3ab47d8cbbc3e3c8af1
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-06 12:00:16 -0300

    fincionamiento del login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 router.get('/', function(req,res,netx){
     res.render('admin/login',{
@@ -7,5 +8,28 @@ router.get('/', function(req,res,netx){
     }); //view/galeria.hbs
 })
 
+router.post('/', async function(req,res,next){
+    try{
+        console.log(req.body)
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+        if (data != undefined){
+            res.redirect('/admin/novedades')
+
+        }else {
+            res.render('admin/login',{
+                layout: 'admin/layout',
+                error:true
+            })
+
+        }
+
+    }catch(error){
+        console.log(error)
+    }
+});
 
 module.exports = router;
