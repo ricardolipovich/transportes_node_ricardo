ce1f5736719cfa2c31d66917c49ec87243b2593f
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-13 10:31:28 -0300

    agregar novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -15,12 +15,39 @@ router.get('/', async function(req,res,netx){
     
 });
 
-router.get('/agregar', ( req, res, next) => {
+
+router.get('/agregar', (req, res, next) => {
     res.render('admin/agregar', {
         layout: 'admin/layout'
     })
 })
+router.post('/agregar', async (req,res,next)=>{
 
+    // console.log(req.body)
+    try{
+        if (req.body.titulo != "" &&  req.body.subtitulo != "" &&  req.body.cuerpo != ""){
+            await novedadesModel.insertNovedades(req.body)
+            res.redirect('/admin/novedades')
+        }else {
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+
+    }catch(error){
+        console.log(error)
+        res.render('admin/agregar',{
+            layout:'admin/layout',
+            error: true,
+            message: 'No se carga la novedad'
+
+    })
+
+
+}
+})
 
 module.exports = router;
 
