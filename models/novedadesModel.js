3983edd900986bdd2551763410682daa94298374
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-15 11:20:24 -0300

    modificar novedades

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -6,24 +6,41 @@ async function getNovedades() {
     return rows;
 }
 
-async function insertNovedades(obj){
-    try{
+async function insertNovedades(obj) {
+    try {
         var query = 'insert into novedades set ?';
-        var rows = await pool.query(query,[obj]);
+        var rows = await pool.query(query, [obj]);
         return rows;
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
         throw error;
     }
 }
 
-async function deleteNovedadByID(id){
+async function deleteNovedadByID(id) {
     var query = 'delete from novedades where id= ?';
-    var rows = await pool.query(query,[id]);
+    var rows = await pool.query(query, [id]);
     return rows;
 }
 
+async function getNovedadesByID(id) {
+    var query = 'select * from novedades where id= ?';
+    var rows = await pool.query(query, [id]);
+    return rows [0];
+
+}
+
+async function modificarNovedadByID(obj,id){
+    try{
+        var query = 'update novedades set ? where id= ?'
+        var row = await pool.query(query,[obj, id]);
+        return row;
+
+    }catch(error){
+        throw error;
+    }
+}
 
 
-module.exports = { getNovedades, insertNovedades, deleteNovedadByID }
+module.exports = { getNovedades, insertNovedades, deleteNovedadByID, getNovedadesByID, modificarNovedadByID }
