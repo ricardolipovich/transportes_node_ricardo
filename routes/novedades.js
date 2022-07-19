a6b5af8501e8557c557841945bd28a0baf79bdeb
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-07-19 14:38:01 -0300

    buscador front

--- routes/novedades.js
+++ routes/novedades.js
@@ -4,12 +4,20 @@ var novedadesModel = require('../models/novedadesModel');
 
 router.get('/', async function(req,res,netx){
 
-    var novedades = await novedadesModel.getNovedades();
+    var novedades;
+
+    if (req.query.q === undefined) {
+        novedades = await novedadesModel.getNovedades();
+    } else {
+        novedades = await novedadesModel.buscarNovedades(req.query.q);
+    }
 
     res.render('novedades',{
         isNovedades:true,
-        novedades
-    }); //view/novedades.hbs
+        novedades,
+        is_search: req.query.q !== undefined,
+        q:req.query.q
+    }); 
 })
 
 
